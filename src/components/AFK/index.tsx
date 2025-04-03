ed38172790618d69a5c5ddfbcb7254a714cea409
Vivi <givvemeee@gmail.com>
2025-04-03 10:22:23 +0900

    Update index.tsx

--- src/components/AFK/index.tsx
+++ src/components/AFK/index.tsx
@@ -2,6 +2,7 @@ import InactivityDetector from "./InactivityDectector";
 import TimerTest from "./test";
 
 export default function AFK() {
+  // Away From Keyboard, 사용자의 움직임이 감지되지 않으면 특정 행위를 하도록 
   return (
     <div className="App">
       {TimerTest()}
